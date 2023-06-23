6a92dc53ae33b4aba28611910bba20d6cc451370
ヒューマンワーキン <coolxiaom95@gmail.com>
2023-06-23 19:04:05 +0800

    fix command pass argument via fields

--- src/main/java/cn/jrmcdp/craftitem/Cmd.java
+++ src/main/java/cn/jrmcdp/craftitem/Cmd.java
@@ -20,211 +20,164 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
 public class Cmd implements CommandExecutor, TabCompleter {
-    private CommandSender sender;
-
-    private String[] args;
 
     public boolean onCommand(CommandSender sender, Command command, String s, String[] args) {
-        this.sender = sender;
-        this.args = args;
-        if (args.length == 0) {
-            Message.getHelp().forEach(sender::sendMessage);
-            return false;
+        if (args.length > 0) {
+            String perm = command.getName() + ".command." + args[0];
+            switch (args[0].toLowerCase()) {
+                case "reload":
+                    if (!sender.hasPermission(perm)) {
+                        return Message.no_permission.msg(sender, perm);
+                    }
+                    return runReload(sender, args);
+                case "category":
+                    if (args.length < 2) break;
+                    if (!sender.hasPermission(perm)) {
+                        return Message.no_permission.msg(sender, perm);
+                    }
+                    return runCategory(sender, args);
+                case "open":
+                    if (args.length < 2) break;
+                    if (!sender.hasPermission(perm)) {
+                        return Message.no_permission.msg(sender, perm);
+                    }
+                    return runOpen(sender, args);
+                case "get":
+                    if (args.length < 2) break;
+                    if (!sender.hasPermission(perm)) {
+                        return Message.no_permission.msg(sender, perm);
+                    }
+                    return runGet(sender, args);
+                case "edit":
+                    if (args.length < 2) break;
+                    if (!sender.hasPermission(perm)) {
+                        return Message.no_permission.msg(sender, perm);
+                    }
+                    return runEdit(sender, args);
+                case "create":
+                    if (args.length < 2) break;
+                    if (!sender.hasPermission(perm)) {
+                        return Message.no_permission.msg(sender, perm);
+                    }
+                    return runCreate(sender, args);
+                case "delete":
+                    if (args.length < 2) break;
+                    if (!sender.hasPermission(perm)) {
+                        return Message.no_permission.msg(sender, perm);
+                    }
+                    return runDelete(sender, args);
+                default:
+                    break;
+            }
         }
-        String prem = command.getName() + ".command." + args[0];
-        switch (args[0].toLowerCase()) {
-            case "reload":
-                if (!sender.hasPermission(prem)) {
-                    sender.sendMessage(Message.getPrefix() + Message.getNoPermission()
-                            .replace("<Prem>", prem)
-                    );
-                    return false;
-                }
-                return runReload();
-            case "category":
-                if (args.length < 2) break;
-                if (!sender.hasPermission(prem)) {
-                    sender.sendMessage(Message.getPrefix() + Message.getNoPermission()
-                            .replace("<Prem>", prem)
-                    );
-                    return false;
-                }
-                return runCategory();
-            case "open":
-                if (args.length < 2) break;
-                if (!sender.hasPermission(prem)) {
-                    sender.sendMessage(Message.getPrefix() + Message.getNoPermission()
-                            .replace("<Prem>", prem)
-                    );
-                    return false;
-                }
-                return runOpen();
-            case "get":
-                if (args.length < 2) break;
-                if (!sender.hasPermission(prem)) {
-                    sender.sendMessage(Message.getPrefix() + Message.getNoPermission()
-                            .replace("<Prem>", prem)
-                    );
-                    return false;
-                }
-                return runGet();
-            case "edit":
-                if (args.length < 2) break;
-                if (!sender.hasPermission(prem)) {
-                    sender.sendMessage(Message.getPrefix() + Message.getNoPermission()
-                            .replace("<Prem>", prem)
-                    );
-                    return false;
-                }
-                return runEdit();
-            case "create":
-                if (args.length < 2) break;
-                if (!sender.hasPermission(prem)) {
-                    sender.sendMessage(Message.getPrefix() + Message.getNoPermission()
-                            .replace("<Prem>", prem)
-                    );
-                    return false;
-                }
-                return runCreate();
-            case "delete":
-                if (args.length < 2) break;
-                if (!sender.hasPermission(prem)) {
-                    sender.sendMessage(Message.getPrefix() + Message.getNoPermission()
-                            .replace("<Prem>", prem)
-                    );
-                    return false;
-                }
-                return runDelete();
-            default:
-                for (String line : Message.getHelp())
-                    sender.sendMessage(line);
-                return false;
-        }
-        Message.getHelp().forEach(sender::sendMessage);
-        return false;
+        return Message.help.msg0(sender);
     }
 
-    private boolean runDelete() {
-        if (!(this.sender instanceof Player)) {
-            this.sender.sendMessage(Message.getPrefix() + Message.getNoPlayer());
-            return false;
+    private boolean runDelete(CommandSender sender, String[] args) {
+        if (!(sender instanceof Player)) {
+            return Message.no_player.msg(sender);
         }
-        Player player = (Player)this.sender;
-        CraftData craftData = Craft.getCraftData(this.args[1]);
+        Player player = (Player)sender;
+        CraftData craftData = Craft.getCraftData(args[1]);
         if (craftData == null) {
-            player.sendMessage(Message.getPrefix() + "§c未找到 §e" + args[1]);
-            return false;
+            return Message.craft__not_found.msg(player, args[1]);
         }
         Craft.delete(args[1]);
-        player.sendMessage(Message.getPrefix() + "§a成功删除 §e" + args[1]);
-        return true;
+        return Message.delete__done.msg(player, args[1]);
     }
 
-    private boolean runCreate() {
-        if (!(this.sender instanceof Player)) {
-            this.sender.sendMessage(Message.getPrefix() + Message.getNoPlayer());
-            return false;
+    private boolean runCreate(CommandSender sender, String[] args) {
+        if (!(sender instanceof Player)) {
+            return Message.no_player.msg(sender);
         }
-        Player player = (Player)this.sender;
-        CraftData craftData = Craft.getCraftData(this.args[1]);
+        Player player = (Player)sender;
+        CraftData craftData = Craft.getCraftData(args[1]);
         if (craftData != null) {
-            player.sendMessage(Message.getPrefix() + "§c已存在 §e" + args[1]);
-            return false;
+            return Message.create__found.msg(player, args[1]);
         }
-        craftData = new CraftData(new ArrayList(), 75, Arrays.asList(5, 10, 20), 188, new ItemStack(Material.COBBLESTONE), new ArrayList(), new ArrayList());
+        craftData = new CraftData(new ArrayList<>(), 75, Arrays.asList(5, 10, 20), 188, new ItemStack(Material.COBBLESTONE), new ArrayList<>(), new ArrayList<>());
         Craft.save(args[1], craftData);
-        player.openInventory(new EditHolder(this.args[1], craftData).buildGui());
+        player.openInventory(EditHolder.buildGui(args[1], craftData));
         return true;
     }
 
-    private boolean runEdit() {
-        if (!(this.sender instanceof Player)) {
-            this.sender.sendMessage(Message.getPrefix() + Message.getNoPlayer());
-            return false;
+    private boolean runEdit(CommandSender sender, String[] args) {
+        if (!(sender instanceof Player)) {
+            return Message.no_player.msg(sender);
         }
-        Player player = (Player)this.sender;
-        CraftData craftData = Craft.getCraftData(this.args[1]);
+        Player player = (Player)sender;
+        CraftData craftData = Craft.getCraftData(args[1]);
         if (craftData == null) {
-            player.sendMessage(Message.getPrefix() + "§c未找到 §e" + args[1]);
-            return false;
+            return Message.craft__not_found.msg(player, args[1]);
         }
-        player.openInventory(new EditHolder(this.args[1], craftData).buildGui());
+        player.openInventory(EditHolder.buildGui(args[1], craftData));
         return true;
     }
 
-    private boolean runOpen() {
+    private boolean runOpen(CommandSender sender, String[] args) {
         Player player;
-        if (this.args.length >= 3) {
-            player = Bukkit.getPlayer(this.args[2]);
+        if (args.length >= 3) {
+            player = Bukkit.getPlayer(args[2]);
             if (player == null || !player.isOnline()) {
-                this.sender.sendMessage(Message.getPrefix() + "§c玩家未在线或不存在");
-                return false;
+                return Message.not_online.msg(sender);
             }
         } else {
-            if (!(this.sender instanceof Player)) {
-                this.sender.sendMessage(Message.getPrefix() + Message.getNoPlayer());
-                return false;
+            if (!(sender instanceof Player)) {
+                return Message.no_player.msg(sender);
             }
-            player = (Player)this.sender;
+            player = (Player)sender;
         }
-        CraftData craftData = Craft.getCraftData(this.args[1]);
+        CraftData craftData = Craft.getCraftData(args[1]);
         if (craftData == null) {
-            player.sendMessage(Message.getPrefix() + "§c未找到 §e" + args[1]);
-            return false;
+            return Message.craft__not_found.msg(player, args[1]);
         }
         Bukkit.getScheduler().runTaskLater(CraftItem.getPlugin(), () -> player.openInventory(Gui.buildGui(DataManager.getOrCreatePlayerData(player), args[1], craftData)), 1);
         return false;
     }
 
-    private boolean runCategory() {
+    private boolean runCategory(CommandSender sender, String[] args) {
         Player player;
-        if (this.args.length >= 3) {
-            player = Bukkit.getPlayer(this.args[2]);
+        if (args.length >= 3) {
+            player = Bukkit.getPlayer(args[2]);
             if (player == null || !player.isOnline()) {
-                this.sender.sendMessage(Message.getPrefix() + "§c玩家未在线或不存在");
-                return false;
+                return Message.not_online.msg(sender);
             }
         } else {
-            if (!(this.sender instanceof Player)) {
-                this.sender.sendMessage(Message.getPrefix() + Message.getNoPlayer());
-                return false;
+            if (!(sender instanceof Player)) {
+                return Message.no_player.msg(sender);
             }
-            player = (Player)this.sender;
+            player = (Player)sender;
         }
         List<String> list = Config.getCategory().get(args[1]);
         if (list == null) {
-            player.sendMessage(Message.getPrefix() + "§c未找到 §e" + args[1]);
-            return false;
+            return Message.category__not_found.msg(player, args[1]);
         }
         Bukkit.getScheduler().runTaskLater(CraftItem.getPlugin(), () -> Category.openGui(DataManager.getOrCreatePlayerData(player), args[1], list, 0), 1);
         return true;
     }
 
-    private boolean runGet() {
+    private boolean runGet(CommandSender sender, String[] args) {
         Player player;
-        if (this.args.length >= 3) {
-            player = Bukkit.getPlayer(this.args[2]);
+        if (args.length >= 3) {
+            player = Bukkit.getPlayer(args[2]);
             if (player == null || !player.isOnline()) {
-                this.sender.sendMessage(Message.getPrefix() + "§c玩家未在线或不存在");
-                return false;
+                return Message.not_online.msg(sender);
             }
         } else {
-            if (!(this.sender instanceof Player)) {
-                this.sender.sendMessage(Message.getPrefix() + Message.getNoPlayer());
-                return false;
+            if (!(sender instanceof Player)) {
+                return Message.no_player.msg(sender);
             }
-            player = (Player)this.sender;
+            player = (Player)sender;
         }
-        CraftData craftData = Craft.getCraftData(this.args[1]);
+        CraftData craftData = Craft.getCraftData(args[1]);
         if (craftData == null) {
-            player.sendMessage(Message.getPrefix() + "§c未找到 §e" + args[1]);
-            return false;
+            return Message.craft__not_found.msg(player, args[1]);
         }
-        player.sendMessage(Message.getPrefix() + "§a成功锻造出了 §e" + cn.jrmcdp.craftitem.config.Material.getItemName(craftData.getDisplayItem()));
+        Message.craft__success.msg(player, cn.jrmcdp.craftitem.config.Material.getItemName(craftData.getDisplayItem()));
         for (ItemStack item : craftData.getItems()) {
             for (ItemStack add : player.getInventory().addItem(new ItemStack[] { item }).values()) {
                 player.getWorld().dropItem(player.getLocation(), add);
-                player.sendMessage(Message.getPrefix() + "§c背包已满 §d" + cn.jrmcdp.craftitem.config.Material.getItemName(add) + "§ex" + add.getAmount() + " §c掉了出来");
+                Message.full_inventory.msg(player, cn.jrmcdp.craftitem.config.Material.getItemName(add), add.getAmount());
             }
         }
         for (String str : craftData.getCommands()) {
@@ -234,19 +187,18 @@ public class Cmd implements CommandExecutor, TabCompleter {
         return true;
     }
 
-    private boolean runReload() {
+    private boolean runReload(CommandSender sender, String[] args) {
         Message.reload();
         cn.jrmcdp.craftitem.config.Material.reload();
         Config.reload();
         Craft.reload();
         Gui.reload();
         Category.reload();
-        this.sender.sendMessage(Message.getPrefix() + Message.getReload());
-        return true;
+        return Message.reload.msg(sender);
     }
 
     @Override
-    public List<String> onTabComplete(CommandSender commandSender, Command command, String s, String[] args) {
+    public List<String> onTabComplete(CommandSender sender, Command command, String s, String[] args) {
         switch (args.length) {
             case 1 : {
                 List<String> list = new ArrayList<String>() {{
@@ -262,18 +214,14 @@ public class Cmd implements CommandExecutor, TabCompleter {
                 return list;
             }
             case 2 : {
-                switch (args[0].toLowerCase()) {
-                    case "category" : {
-                        List<String> list = new ArrayList<>(Config.getCategory().keySet());
-                        list.removeIf(next -> !next.toLowerCase().startsWith(args[1].toLowerCase()));
-                        return list;
-                    }
-                    default : {
-                        List<String> list = new ArrayList<>(Craft.getCraftDataMap().keySet());
-                        list.removeIf(next -> !next.toLowerCase().startsWith(args[1].toLowerCase()));
-                        return list;
-                    }
+                if (args[0].equalsIgnoreCase("category")) {
+                    List<String> list = new ArrayList<>(Config.getCategory().keySet());
+                    list.removeIf(next -> !next.toLowerCase().startsWith(args[1].toLowerCase()));
+                    return list;
                 }
+                List<String> list = new ArrayList<>(Craft.getCraftDataMap().keySet());
+                list.removeIf(next -> !next.toLowerCase().startsWith(args[1].toLowerCase()));
+                return list;
             }
         }
         return null;
