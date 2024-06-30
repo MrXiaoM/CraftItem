933c65e5a8f18f5c601acc4c3fd1a7558a35972d
MrXiaoM <coolxiaom95@gmail.com>
2024-06-30 12:18:09 +0800

    use `.kts` build script

---
+++ build.gradle.kts
@@ -0,0 +1,82 @@
+plugins {
+    java
+    `maven-publish`
+    id ("com.github.johnrengelman.shadow") version "7.0.0"
+}
+
+group = "cn.jrmcdp"
+version = "1.0.8"
+val targetJavaVersion = 8
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+    maven("https://repo.extendedclip.com/content/repositories/placeholderapi/")
+    maven("https://repo.codemc.io/repository/maven-public/")
+    maven("https://jitpack.io")
+    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots")
+}
+
+dependencies {
+    // 只在 cn.jrmcdp.craftitem.minigames.OnPaper#onJump 用到了 PaperAPI
+    compileOnly("com.destroystokyo.paper:paper-api:1.16.5-R0.1-SNAPSHOT")
+    compileOnly("com.github.MilkBowl:VaultAPI:1.7")
+    compileOnly("me.clip:placeholderapi:2.11.2")
+    compileOnly("com.comphenix.protocol:ProtocolLib:4.8.0")
+
+    implementation("net.kyori:adventure-api:4.15.0")
+    implementation("net.kyori:adventure-platform-bukkit:4.3.2")
+    implementation("net.kyori:adventure-text-minimessage:4.14.0")
+    implementation("net.objecthunter:exp4j:0.4.8")
+}
+
+java {
+    withSourcesJar()
+    val javaVersion = JavaVersion.toVersion(targetJavaVersion)
+    if (JavaVersion.current() < javaVersion) {
+        val lang = JavaLanguageVersion.of(targetJavaVersion)
+        toolchain.languageVersion.set(lang)
+    }
+    sourceCompatibility = javaVersion
+    targetCompatibility = javaVersion
+}
+tasks {
+    build {
+        dependsOn(shadowJar)
+    }
+    shadowJar {
+        from("LICENSE")
+        archiveClassifier.set("")
+        mapOf(
+            "net.kyori" to "kyori",
+            "net.objecthunter.exp4j" to "exp4j"
+        ).forEach { (original, target) ->
+            relocate(original, "cn.jrmcdp.craftitem.utils.$target")
+        }
+    }
+    processResources {
+        duplicatesStrategy = DuplicatesStrategy.INCLUDE
+        from(sourceSets.main.get().resources.srcDirs) {
+            expand(mapOf("version" to version))
+            include("plugin.yml")
+        }
+    }
+    withType<JavaCompile>().configureEach {
+        options.encoding = "UTF-8"
+        if (targetJavaVersion >= 10 || JavaVersion.current().isJava10Compatible) {
+            options.release.set(targetJavaVersion)
+        }
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenRelease") {
+            from(components.getByName("java"))
+            groupId = project.group.toString()
+            artifactId = project.name
+            version = project.version.toString()
+        }
+    }
+}
