e8122058ab2b58c7a19c321ed85dc708a7b47e97
人間工作 <coolxiaom95@gmail.com>
2024-08-08 21:19:32 +0800

    log warning if can't read craft data

--- src/main/java/cn/jrmcdp/craftitem/config/Craft.java
+++ src/main/java/cn/jrmcdp/craftitem/config/Craft.java
@@ -1,20 +1,30 @@
 package cn.jrmcdp.craftitem.config;
 
+import cn.jrmcdp.craftitem.CraftItem;
 import cn.jrmcdp.craftitem.data.CraftData;
 
 import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Logger;
+
 import org.bukkit.configuration.file.YamlConfiguration;
 
 public class Craft {
     private static YamlConfiguration config;
 
-    private static HashMap<String, CraftData> craftDataMap;
+    private static final Map<String, CraftData> craftDataMap = new HashMap<>();
 
     public static void reload() {
         config = FileConfig.Craft.getConfig();
-        craftDataMap = new HashMap<>();
+        craftDataMap.clear();
         for (String key : config.getKeys(false)) {
-            craftDataMap.put(key, (CraftData) config.get(key, CraftData.class));
+            Object object = config.get(key, null);
+            if (object instanceof CraftData) {
+                craftDataMap.put(key, (CraftData) object);
+            } else {
+                Logger logger = CraftItem.getPlugin().getLogger();
+                logger.warning("无法读取 Craft.yml 的项目 " + key + ": " + object);
+            }
         }
     }
 
@@ -22,7 +32,7 @@ public class Craft {
         return config;
     }
 
-    public static HashMap<String, CraftData> getCraftDataMap() {
+    public static Map<String, CraftData> getCraftDataMap() {
         return craftDataMap;
     }
 
