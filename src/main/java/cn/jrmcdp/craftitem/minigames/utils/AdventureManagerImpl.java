ce39779ef6b17d2c22715fe0306755abb1e8cca8
人間工作 <coolxiaom95@gmail.com>
2024-11-30 21:09:10 +0800

    move MiniMessageConvert to specific class

--- src/main/java/cn/jrmcdp/craftitem/minigames/utils/AdventureManagerImpl.java
+++ src/main/java/cn/jrmcdp/craftitem/minigames/utils/AdventureManagerImpl.java
@@ -5,7 +5,6 @@ import net.kyori.adventure.key.Key;
 import net.kyori.adventure.platform.bukkit.BukkitAudiences;
 import net.kyori.adventure.sound.Sound;
 import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.minimessage.MiniMessage;
 import net.kyori.adventure.title.Title;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -13,14 +12,14 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import java.time.Duration;
 
+import static cn.jrmcdp.craftitem.utils.MiniMessageConvert.miniMessage;
+
 public class AdventureManagerImpl {
     private final BukkitAudiences adventure;
-    private final MiniMessage miniMessage;
     private static AdventureManagerImpl instance;
 
     private AdventureManagerImpl(JavaPlugin plugin) {
         this.adventure = BukkitAudiences.create(plugin);
-        this.miniMessage = MiniMessage.builder().build();
         instance = this;
     }
 
@@ -32,13 +31,6 @@ public class AdventureManagerImpl {
 
     public static AdventureManagerImpl getInstance() {
         return instance;
-    }
-
-    public Component miniMessage(String text) {
-        if (text == null) {
-            return Component.empty();
-        }
-        return miniMessage.deserialize(legacyToMiniMessage(text));
     }
 
     public void sendMessage(CommandSender sender, String message) {
@@ -68,79 +60,6 @@ public class AdventureManagerImpl {
         Sound sound = Sound.sound(key, source, volume, pitch);
         Audience au = adventure.player(player);
         au.playSound(sound);
-    }
-
-    public String legacyToMiniMessage(String legacy) {
-        StringBuilder stringBuilder = new StringBuilder();
-        char[] chars = legacy.toCharArray();
-        for (int i = 0; i < chars.length; i++) {
-            if (!isColorCode(chars[i])) {
-                stringBuilder.append(chars[i]);
-                continue;
-            }
-            if (i + 1 >= chars.length) {
-                stringBuilder.append(chars[i]);
-                continue;
-            }
-            switch (chars[i+1]) {
-                case '0': stringBuilder.append("<black>"); break;
-                case '1': stringBuilder.append("<dark_blue>"); break;
-                case '2': stringBuilder.append("<dark_green>"); break;
-                case '3': stringBuilder.append("<dark_aqua>"); break;
-                case '4': stringBuilder.append("<dark_red>"); break;
-                case '5': stringBuilder.append("<dark_purple>"); break;
-                case '6': stringBuilder.append("<gold>"); break;
-                case '7': stringBuilder.append("<gray>"); break;
-                case '8': stringBuilder.append("<dark_gray>"); break;
-                case '9': stringBuilder.append("<blue>"); break;
-                case 'a': stringBuilder.append("<green>"); break;
-                case 'b': stringBuilder.append("<aqua>"); break;
-                case 'c': stringBuilder.append("<red>"); break;
-                case 'd': stringBuilder.append("<light_purple>"); break;
-                case 'e': stringBuilder.append("<yellow>"); break;
-                case 'f': stringBuilder.append("<white>"); break;
-                case 'r': stringBuilder.append("<reset><!i>"); break;
-                case 'l': stringBuilder.append("<b>"); break;
-                case 'm': stringBuilder.append("<st>"); break;
-                case 'o': stringBuilder.append("<i>"); break;
-                case 'n': stringBuilder.append("<u>"); break;
-                case 'k': stringBuilder.append("<obf>"); break;
-                case 'x': {
-                    if (i + 13 >= chars.length
-                            || !isColorCode(chars[i+2])
-                            || !isColorCode(chars[i+4])
-                            || !isColorCode(chars[i+6])
-                            || !isColorCode(chars[i+8])
-                            || !isColorCode(chars[i+10])
-                            || !isColorCode(chars[i+12])) {
-                        stringBuilder.append(chars[i]);
-                        continue;
-                    }
-                    stringBuilder
-                            .append("<#")
-                            .append(chars[i+3])
-                            .append(chars[i+5])
-                            .append(chars[i+7])
-                            .append(chars[i+9])
-                            .append(chars[i+11])
-                            .append(chars[i+13])
-                            .append(">");
-                    i += 12;
-                    break;
-                }
-                default: {
-                    stringBuilder.append(chars[i]);
-                    continue;
-                }
-            }
-            i++;
-        }
-        return stringBuilder.toString();
-    }
-
-    @SuppressWarnings("BooleanMethodIsAlwaysInverted")
-    public boolean isColorCode(char c) {
-        return c == '§' || c == '&';
     }
 
 }
