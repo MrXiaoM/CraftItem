bfbc5c28e8112bdf896a1546c9ffcf98a565a59f
人間工作 <coolxiaom95@gmail.com>
2025-05-11 02:33:04 +0800

    Update build.gradle.kts

--- paper/build.gradle.kts
+++ paper/build.gradle.kts
@@ -4,8 +4,8 @@ plugins {
 version = "1.0-SNAPSHOT"
 dependencies {
     compileOnly("com.destroystokyo.paper:paper-api:1.16.5-R0.1-SNAPSHOT")
-    compileOnly("top.mrxiaom:PluginBase:1.3.8")
-    compileOnly("net.kyori:adventure-api:4.17.0")
+    compileOnly("top.mrxiaom:PluginBase:1.4.1")
+    compileOnly("net.kyori:adventure-api:4.20.0")
     compileOnly("net.kyori:adventure-platform-bukkit:4.3.4")
-    compileOnly("net.kyori:adventure-text-minimessage:4.17.0")
+    compileOnly("net.kyori:adventure-text-minimessage:4.20.0")
 }
