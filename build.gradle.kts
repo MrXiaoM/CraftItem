db2e79eab10a268315a4c4938e3db69b1bfabc3c
MrXiaoM <coolxiaom95@gmail.com>
2025-08-17 22:45:12 +0800

    add plain serializer for mv compatibility

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,6 +38,7 @@ dependencies {
     library("net.kyori:adventure-api:4.22.0")
     library("net.kyori:adventure-platform-bukkit:4.4.0")
     library("net.kyori:adventure-text-minimessage:4.22.0")
+    library("net.kyori:adventure-text-serializer-plain:4.22.0")
 
     implementation("de.tr7zw:item-nbt-api:2.15.2-SNAPSHOT")
     implementation("com.github.technicallycoded:FoliaLib:0.4.4")
