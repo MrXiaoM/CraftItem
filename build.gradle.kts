5a18d2c85bdfc8910f88faadc1173118249433bc
MrXiaoM <coolxiaom95@gmail.com>
2026-03-29 18:57:33 +0800

    update 2.1.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ buildscript {
 val base = top.mrxiaom.gradle.LibraryHelper(project)
 
 group = "cn.jrmcdp"
-version = "2.1.0"
+version = "2.1.1"
 
 val pluginBaseModules = base.modules.run{ listOf(library, paper, l10n, actions, gui, misc) }
 val targetJavaVersion = 8
